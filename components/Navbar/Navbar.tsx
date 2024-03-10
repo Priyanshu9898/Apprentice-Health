d7eaba0f2f3f3b1a8ac1b565f4848a2caef8a1be
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-03-10 14:40:14 +0530

    Hover effect added on Navbar Links

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -21,12 +21,12 @@ const NavbarComponent = () => {
 
         {/* Navbar Menu Items */}
         <div className="hidden md:flex md:items-center md:justify-between md:gap-x-6 lg:gap-x-10 text-[#828d9c] font-semibold">
-          <Link href="/workflow">Workflow</Link>
-          <Link href="/scheduling">Scheduling</Link>
-          <Link href="/analytics">Analytics</Link>
-          <Link href="/smartHospital">Smart Hospital</Link>
-          <Link href="/resources">Resources</Link>
-          <Link href="/about">About Us</Link>
+          <Link href="/workflow" className="hover-link">Workflow</Link>
+          <Link href="/scheduling" className="hover-link">Scheduling</Link>
+          <Link href="/analytics" className="hover-link">Analytics</Link>
+          <Link href="/smartHospital" className="hover-link">Smart Hospital</Link>
+          <Link href="/resources" className="hover-link">Resources</Link>
+          <Link href="/about" className="hover-link">About Us</Link>
         </div>
       </div>
     </nav>
